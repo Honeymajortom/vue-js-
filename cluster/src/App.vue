b509e9d319b49118d54228c3fc2bc2ea4a969719
Amit <waghmare_a@fevcd>
2023-09-24 20:25:54 +0530

    vue project

---
+++ cluster/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="background">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+
+  }
+}
+</script>
+
+<style>
+.background {
+  background-color: black;
+}
+</style>
